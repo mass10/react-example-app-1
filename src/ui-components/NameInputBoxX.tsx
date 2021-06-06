936f9d6d1407285858c7a1dfa166474aecd00d91
Irisawa Masaru <irisawa.masaru@gmail.com>
2021-06-06 16:37:51 +0900

    -

--- src/ui-components/NameInputBoxX.tsx
+++ src/ui-components/NameInputBoxX.tsx
@@ -10,13 +10,17 @@ const privateStateStash = {
 }
 
 export const NameInputBox = (): JSX.Element => {
+
 	// privateStateStash.name = "Jimi";
+
 	console.log(`${new Date()} [TRACE] <NameInputBox()> called. name is [${privateStateStash.name}]`);
+
 	const onChangeTextValue = (e: any) => {
 		console.log(`${new Date()} [TRACE] new value is [${e.target.value}]`);
 		privateStateStash.name = e.target.value;
 	}
 
+	// ただの変数では Reactive な挙動にならない。
 	return (
 		<section>
 			<input name="name1" style={{fontSize: "20px", padding: "8px"}} value={privateStateStash.name} onChange={onChangeTextValue} />
